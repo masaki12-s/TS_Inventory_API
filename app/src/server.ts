b0e427ee8b9511b2af5fe405d757714da179915d
masaki12-s <asam.archer1112@yahoo.ne.jp>
2024-03-11 18:50:43 +0000

    [update] mysql用にすべてアップデート

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,31 +1,32 @@
 import express from 'express';
-//sqlite3
 import dotenv from 'dotenv';
-import { open, Database } from 'sqlite';
-import sqlite3 from 'sqlite3';
+import mysql from "mysql2";
 
 dotenv.config();
 
 const _port = process.env.PORT || "3000";
 const port = parseInt(_port, 10);
 
-// DB接続
-async function connection() {
-    const db = await open({
-        filename: './db.sqlite3',
-        driver: sqlite3.Database
-    });
-    // テーブル作成
-    return db;
-};
+// db接続
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: process.env.DB_USER,
+    port: parseInt(process.env.DB_PORT||"3307", 10),
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+connection.connect();
 
 // テーブル
-async function createTable(db: Database) {
+function initializeTable(conn: mysql.Connection) {
     // drop
-    await db.run('DROP TABLE IF EXISTS stock;');
-    await db.run('CREATE TABLE IF NOT EXISTS stock (name STRING, amount INTEGER, sales REAL);');
-    //const result = await db.get('select * from sqlite_master;');
-    //console.log(result);
+    conn.query('DROP TABLE IF EXISTS stock;');
+    // create
+    conn.query('CREATE TABLE IF NOT EXISTS stock (name VARCHAR(255), amount INT, sales DOUBLE);', (error: any, result: any) => {
+        if (error) {
+            console.log(error);
+        }
+    });
 }
 
 type Stock = {
@@ -34,8 +35,9 @@ type Stock = {
     price: number;
 };
 
-connection().then(async (db) => {
-    await createTable(db);
+
+function main() {
+    initializeTable(connection);
 
     const app: express.Express = express();
     app.use(express.json());
@@ -46,7 +48,9 @@ connection().then(async (db) => {
     });
     const error_message = { "message": "ERROR" };
 
-
+    app.get('/', (req: express.Request, res: express.Response) => {
+        res.json({ message: 'Hello World!' });
+    });
     // 在庫の更新, 作成
     /**
      * POST /v1/stocks
@@ -76,17 +80,28 @@ connection().then(async (db) => {
         if (!stocks.amount) {
             stocks.amount = 1;
         }
-        // DBへの接続
-        const db = await connection();
-        const result = await db.get('SELECT amount FROM stock WHERE name = ?', [stocks.name]);
-        // もし在庫があれば更新
-        if (result) {
-            await db.run('UPDATE stock SET amount = amount + ? WHERE name = ?', [stocks.amount, stocks.name]);
-        } else {
-            await db.run('INSERT INTO stock (name, amount, sales) VALUES (?, ?, ?)', [stocks.name, stocks.amount, 0]);
-        }
-        // レスポンスボディの返却
-        return res.status(201).json(stocks);
+        // クエリの実行
+        connection.query('SELECT amount FROM stock WHERE name = ?', [stocks.name], (error: any, result: any) => {
+            console.log(error, result);
+            if (error) {
+                return res.status(500).json(error_message);
+            } else {
+                if (result) {
+                    connection.query('UPDATE stock SET amount = amount + ? WHERE name = ?', [stocks.amount, stocks.name], (error, result) => {
+                        if (error) {
+                            return res.status(500).json(error_message);
+                        }
+                    });
+                } else {
+                    connection.query('INSERT INTO stock (name, amount, sales) VALUES (?, ?, ?)', [stocks.name, stocks.amount, 0], (error, result) => {
+                        if (error) {
+                            return res.status(500).json(error_message);
+                        }
+                    });
+                }
+                return res.status(201).json(stocks);
+            }
+        });
     });
     // 在庫チェック
     /**
@@ -100,30 +115,44 @@ connection().then(async (db) => {
      * 例: {"apple": 1, "banana": 2, "orange": 0}
      * 在庫が0のものは含まない
     **/
-    app.get('/stocks/:name?', async (req: express.Request, res: express.Response) => {
+    app.get('/stocks/:name?', (req: express.Request, res: express.Response) => {
         // リクエストパラメータの取得・バリデーション
         const name = req.params.name;
         // nameのバリデーション
         if (name && typeof name !== 'string') {
             return res.status(400).json(error_message);
         }
-        const db = await connection();
         let result: Stock[] = [];
         if (name) {
-            result = await db.all('SELECT name, amount FROM stock WHERE name = ?', [name]);
-            return res.json(result);
-        } else {
-            result = await db.all('SELECT name, amount FROM stock WHERE amount > 0 ORDER BY name ASC');
-            // 形式を変換
-            // 例: {"apple": 1, "banana": 2, "orange": 0}
-            const tmp: { [key: string]: number } = {};
-            result.forEach((stock) => {
-                tmp[stock.name] = stock.amount;
+            connection.query('SELECT name, amount FROM stock WHERE name = ?', [name], (error: any, result: any) => {
+                if (result.length === 0) {
+                    return res.json({ [name]: 0 });
+                }
+                return res.json({ [result[0].name]: result[0].amount });
             });
-            return res.json(tmp);
+        } else {
+            connection.query('SELECT name, amount FROM stock WHERE amount > 0 ORDER BY name ASC', (error: any, result: any) => {
+                // 形式を変換
+                // 例: {"apple": 1, "banana": 2, "orange": 0}
+                if (error) {
+                    console.log(error);
+                    return res.status(500).json(error_message);
+                } else {
+                    if (result) {
+                        const tmp: { [key: string]: number } = {};
+                        result.forEach((stock: Stock) => {
+                            tmp[stock.name] = stock.amount;
+                        });
+                        return res.json(tmp);
+                    } else {
+                        return res.status(404).json(error_message);
+                    
+                    }
+                }
+            });
         }
-    }
-    );
+    });
+
     // 販売
     /**
      * POST /v1/sales
@@ -140,40 +169,40 @@ connection().then(async (db) => {
      * http response body
      * request bodyと同じ
      **/
-    app.post('/sales', async (req: express.Request, res: express.Response) => {
-        // リクエストボディの取得・バリデーション
-        const sales = req.body;
-        res.setHeader('Location', `/v1/sales/${sales.name}`);
-        if (!sales.name) {
-            return res.status(400).json(error_message);
-        }
-        // int型かつ正の整数であるかのチェック
-        if (sales.amount && (!Number.isInteger(sales.amount) || sales.amount < 0)) {
-            return res.status(400).json(error_message);
-        }
-        // int型かつ正の整数であるかのチェック
-        if (sales.price && (!Number.isInteger(sales.price) || sales.price < 0)) {
-            return res.status(400).json(error_message);
-        }
-        // amountの初期化
-        let amount = 1;
-        // もしamountが指定されていればamountを更新
-        if (sales.amount) {
-            amount = sales.amount;
-        }
-        const db = await connection();
-        const result = await db.get('SELECT amount FROM stock WHERE name = ?', [sales.name]);
-        if (!result || result.amount < amount) {
-            return res.status(400).json(error_message);
-        } else {
-            await db.run('UPDATE stock SET amount = amount - ? WHERE name = ?', [amount, sales.name]);
-            if (sales.price) {
-                await db.run('UPDATE stock SET sales = sales + ? WHERE name = ?', [sales.price * amount, sales.name]);
-            }
-            return res.status(201).json(sales);
-        }
-    }
-    );
+    // app.post('/sales', async (req: express.Request, res: express.Response) => {
+    //     // リクエストボディの取得・バリデーション
+    //     const sales = req.body;
+    //     res.setHeader('Location', `/v1/sales/${sales.name}`);
+    //     if (!sales.name) {
+    //         return res.status(400).json(error_message);
+    //     }
+    //     // int型かつ正の整数であるかのチェック
+    //     if (sales.amount && (!Number.isInteger(sales.amount) || sales.amount < 0)) {
+    //         return res.status(400).json(error_message);
+    //     }
+    //     // int型かつ正の整数であるかのチェック
+    //     if (sales.price && (!Number.isInteger(sales.price) || sales.price < 0)) {
+    //         return res.status(400).json(error_message);
+    //     }
+    //     // amountの初期化
+    //     let amount = 1;
+    //     // もしamountが指定されていればamountを更新
+    //     if (sales.amount) {
+    //         amount = sales.amount;
+    //     }
+    //     const db = await connection();
+    //     const result = await db.get('SELECT amount FROM stock WHERE name = ?', [sales.name]);
+    //     if (!result || result.amount < amount) {
+    //         return res.status(400).json(error_message);
+    //     } else {
+    //         await db.run('UPDATE stock SET amount = amount - ? WHERE name = ?', [amount, sales.name]);
+    //         if (sales.price) {
+    //             await db.run('UPDATE stock SET sales = sales + ? WHERE name = ?', [sales.price * amount, sales.name]);
+    //         }
+    //         return res.status(201).json(sales);
+    //     }
+    // }
+    // );
 
     // 売り上げチェック
     /**
@@ -185,33 +214,35 @@ connection().then(async (db) => {
      * }
      * 
      **/
-    app.get('/sales', async (req: express.Request, res: express.Response) => {
-        // リクエストパラメータの取得・バリデーション
-        // もしパラメータがあればエラー
-        if (Object.keys(req.query).length !== 0) {
-            return res.status(400).json(error_message);
-        }
-        const db = await connection();
-        const sales = await db.get('SELECT SUM(sales) AS sales FROM stock');
-        // 小数であれば小数第二位まで、整数であれば小数第一位まで
-        const result = { sales: sales.sales.toFixed(sales.sales % 1 === 0 ? 1 : 2) };
-        return res.json(result);
-    }
-    );
+    // app.get('/sales', async (req: express.Request, res: express.Response) => {
+    //     // リクエストパラメータの取得・バリデーション
+    //     // もしパラメータがあればエラー
+    //     if (Object.keys(req.query).length !== 0) {
+    //         return res.status(400).json(error_message);
+    //     }
+    //     const db = await connection();
+    //     const sales = await db.get('SELECT SUM(sales) AS sales FROM stock');
+    //     // 小数であれば小数第二位まで、整数であれば小数第一位まで
+    //     const result = { sales: sales.sales.toFixed(sales.sales % 1 === 0 ? 1 : 2) };
+    //     return res.json(result);
+    // }
+    // );
 
     // 全削除
     /**
      * DELETE /v1/stocks
     **/
-    app.delete('/stocks', async (req: express.Request, res: express.Response) => {
-        // リクエストパラメータの取得・バリデーション
-        // もしパラメータがあればエラー
-        if (Object.keys(req.query).length !== 0) {
-            return res.status(400).json(error_message);
-        }
-        const db = await connection();
-        await db.run('DELETE FROM stock');
-        return res.status(204).end();
-    }
-    );
-});
+    // app.delete('/stocks', async (req: express.Request, res: express.Response) => {
+    //     // リクエストパラメータの取得・バリデーション
+    //     // もしパラメータがあればエラー
+    //     if (Object.keys(req.query).length !== 0) {
+    //         return res.status(400).json(error_message);
+    //     }
+    //     const db = await connection();
+    //     await db.run('DELETE FROM stock');
+    //     return res.status(204).end();
+    // }
+    // );
+}
+
+main();
