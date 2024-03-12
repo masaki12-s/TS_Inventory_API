35d1fbbc96df921f307b4db20e8c7ed08bdf36aa
masaki12-s <asam.archer1112@yahoo.ne.jp>
2024-03-12 12:35:53 +0000

    rm

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -3,14 +3,11 @@ import { useState, useEffect } from 'react'
 
 import './App.css'
 
-// objectsの例
-// Object { apple: 10, banana: 20, orange: 1 }
-
 function App() {
   const [stocks, setStocks] = useState([Object]);
   useEffect(() => {
     axios
-      .get('http://localhost:3000/stocks')
+      .get('/api/stocks')
       .then((res) => {
         setStocks(res.data)
         console.log(res.data)
@@ -18,19 +15,52 @@ function App() {
         console.log(err)
       })
   }, []);
+  const addStock = async (name: string, amount: number) => {
+    // * http resuest body
+    //  * {
+    // * "name": "string" (required),
+    // * "amount": 0 (optional),
+    // * }
+    await axios
+      .post('/api/stocks', {
+        name: name,
+        amount: amount
+      })
+      .then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
   // inventoryを表示する
   return (
     <>
-      <h2>Inventory</h2>
-      <ul>
-        {Object.keys(stocks).map((key) => {
-          return (
-            <li key={key}>
-              {key}: {stocks[key]}
-            </li>
-          )
-        })}
-      </ul>
+      <h2>Inventory App</h2>
+      <div class="container">
+        <div class="column">
+        <p>在庫一覧</p>
+        a
+          <ul>
+            {Object.keys(stocks).map((key: any) => {
+              return (
+                <li key={key}>
+                  {key}: {stocks[key]}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div class="column">
+          <p>在庫追加</p>
+          <form action="">
+            <label for="name">商品名</label>
+            <input type="text" id="name" name="name" />
+            <label for="amount">個数</label>
+            <input type="number" id="amount" name="amount" />
+            <button onClick={() => addStock('apple', 10)}>追加</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
